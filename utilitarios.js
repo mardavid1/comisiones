0ee20db203085418dd43fad30c42e00f3ed725a2
mardavid1 <mardavid_01@hotmail.com>
2026-07-12 22:11:11 -0400

    Validaciones

---
+++ utilitarios.js
@@ -0,0 +1,18 @@
+function recuperarTexto(idComponente) {
+    let componente = document.getElementById(idComponente);
+    let valor = componente.value;
+    return valor;
+}
+
+function recuperarFloat(idComponente) {
+    let valorTexto = recuperarTexto(idComponente);
+    let valorFloat = parseFloat(valorTexto);
+    return valorFloat;
+}
+
+
+
+
+
+
+
--- ventas.js
+++ ventas.js
@@ -11,30 +11,88 @@ function calcularComision(numeroVentas, precioProducto) {
 
 }
 
+///Validar ventas
+
+//function validarVentas() {
+    //Para validacion primero recuperamos el texto de la cada de texto
+    // let numeroVentasStr = recuperarTexto('txtVentas');
+    // if (numeroVentasStr.length >= 5) {
+    // alert('Maximo 5 caracteres');
+    // return false;
+    // } else {
+    //  return true;
+    //}
+    //}
+
+
+
+
 function calcular() {
+
+    if (validarVentas() == false) {
+        return;
+    };
     //Recuperamos propiedades de cajas de texto
-    let componenteSueldoBase = document.getElementById("txtSueldoBase");
-    let componenteVentas = document.getElementById('txtVentas');
-    let componentePrecio = document.getElementById('txtPrecio');
+    //let componenteSueldoBase = document.getElementById("txtSueldoBase");
+    //let componenteVentas = document.getElementById('txtVentas');
+    //let componentePrecio = document.getElementById('txtPrecio');
 
     //recuperamos el valor de las cajas de texto
-    let sueldoBaseStr = componenteSueldoBase.value;
-    let numeroVentasStr = componenteVentas.value;
-    let PrecioProductoStr = componentePrecio.value;
+    //let sueldoBaseStr = componenteSueldoBase.value;
+
+
+    //let sueldoBaseStr=recuperarTexto('txtSueldoBase');
+    //let numeroVentasStr=recuperarTexto('txtVentas');
+    //let PrecioProductoStr=recuperarTexto('txtPrecio');
+
+
+    //let numeroVentasStr = componenteVentas.value;
+    //let PrecioProductoStr = componentePrecio.value;
 
     //Convertimos el texto a numero
-    let sueldoBase = parseFloat(sueldoBaseStr);
-    let numeroVentas = parseFloat(numeroVentasStr);
-    let PrecioProducto = parseFloat(PrecioProductoStr);
+    // /////////////////////////////////////////////////////////////////
 
-    let comision=calcularComision(numeroVentas,PrecioProducto);
-    let total=sueldoBase+comision;
 
-    let spSueldoBase=document.getElementById('spSueldoBase');
-    let spComision=document.getElementById('spComision');
-    let spTotal=document.getElementById('spTotal');
 
-    spSueldoBase.textContent=sueldoBase;
-    spComision.textContent=comision;
-    spTotal.textContent=total;
+
+
+
+    let sueldoBase = recuperarFloat('txtSueldoBase');
+    let numeroVentas = recuperarFloat('txtVentas');
+    let PrecioProducto = recuperarFloat('txtPrecio');
+
+    let comision = calcularComision(numeroVentas, PrecioProducto);
+    let total = sueldoBase + comision;
+
+    let spSueldoBase = document.getElementById('spSueldoBase');
+    let spComision = document.getElementById('spComision');
+    let spTotal = document.getElementById('spTotal');
+
+    spSueldoBase.textContent = sueldoBase;
+    spComision.textContent = comision;
+    spTotal.textContent = total;
+}
+
+
+function validarNumero(input) {
+
+    const mensaje = document.getElementById("error_" + input.id);
+    mensaje.textContent = "";
+
+    if (input.value.trim() === "") {
+        mensaje.textContent = "Este campo es obligatorio.";
+        return false;
+    }
+
+    if (!/^\d+$/.test(input.value)) {
+        mensaje.textContent = "Solo se permiten números.";
+        return false;
+    }
+
+    if (input.value.length > 5) {
+        mensaje.textContent = "Máximo 5 dígitos.";
+        return false;
+    }
+
+    return true;
 }
